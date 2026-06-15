516cb327fcff17f044a6efde8cbeb4db3b787c25
deceibe_the_world <alisherbolotbekov03@gmail.com>
2026-06-15 18:29:53 +0600

    two commit

--- python.py
+++ python.py
@@ -5,7 +5,15 @@ class User:
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return f"Пользователь {self.name}"
+
 user = User("Алишер")
 
 print(user.name)
+print(user)
 
+# class User:
+#     pass
+# user = User()
+# print(user)
