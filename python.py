3b637fe719183e3da747d98e39fe07b9639d304d
deceibe_the_world <alisherbolotbekov03@gmail.com>
2026-06-15 19:13:39 +0600

    forth com

--- python.py
+++ python.py
@@ -16,3 +16,35 @@ class User:
 
 
 print("Hello world")
+
+# __len__
+# __add__
+
+class Team:
+    def __init__(self,members):
+        self.members = members
+
+    def __len__(self):
+        return len(self.members)
+    
+team = Team("Алишер", "Нурсултан", "Бексултан")
+
+print(len(team))
+
+
+class Money:
+    def __init__(self,amount):
+        self.amount = amount
+
+    def __add__(self, other):
+        return Money(self.amount + other.amount)
+    
+    def __str__(self):
+        return str(self.amount)
+
+m1 = Money(100)
+m2 = Money(50)
+
+print(m1 + m2)
+
+# 150
